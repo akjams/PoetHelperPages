11437184f139b73bfe9f08946ab4b9b9fedf3b43
AK <austin.kemper@live.com>
2021-05-20 13:21:26 -0700

    fix routes with homepage.

---
+++ precache-manifest.4e4b1ec1c6dc9be2f73b514de1793cf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2652f15552f28aaa65ba94203314b64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ab79641eba0121833b1",
+    "url": "./static/css/main.90d1fe61.chunk.css"
+  },
+  {
+    "revision": "5b46cf221f750a47a554",
+    "url": "./static/js/2.bb16c840.chunk.js"
+  },
+  {
+    "revision": "139e16100b8d04c07360f902f8d1d6f3",
+    "url": "./static/js/2.bb16c840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab79641eba0121833b1",
+    "url": "./static/js/main.7ff32edd.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc323b6cf824600501d753c8e2107d84.js"
+  "./precache-manifest.4e4b1ec1c6dc9be2f73b514de1793cf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
